50b84d971d3c94523349b438a6f374b9d10692a8
Charlotte Lin <charlotte.wanchen.lin@gmail.com>
2019-11-13 17:00:33 +0100

    yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create([
+  { name: 'Chinese Taste', address: 'china', phone_number: '123', category: 'chinese' },
+  { name: 'Italian Taste', address: 'italy', phone_number: '456', category: 'italian' },
+  { name: 'Japanese Taste', address: 'japan', phone_number: '789', category: 'japanese' },
+  { name: 'Belgium Taste', address: 'belgium', phone_number: '101', category: 'belgium' },
+  { name: 'French Taste', address: 'france', phone_number: '112', category: 'french' }
+])
