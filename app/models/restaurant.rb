5fabe910fd67ae1f76b96675ca793f11b506a83f
Charlotte Lin <charlotte.wanchen.lin@gmail.com>
2019-11-13 18:32:30 +0100

    complete

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
+  PERMITTED_CATEGORY = ['chinese', 'italian', 'japanese', 'french', 'belgian']
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, presence: true, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
+  validates :category, presence: true, inclusion: { in: PERMITTED_CATEGORY}
   has_many :reviews, dependent: :destroy
 end
